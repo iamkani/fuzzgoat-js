612169a62cd148f2d451df4ad74e7a2fc5fc11d2
Hamid <hamid.azimy@gmail.com>
2023-10-25 16:17:55 -0300

    Update cyd.py

--- .github/cyd.py
+++ .github/cyd.py
@@ -33,6 +33,8 @@ response = requests.post(f'{base_url}users/{user_id}/projects/by_path/', data, h
 
 print(response.content.decode())
 
+data['repo'] = response.json()['id']
+
 response = requests.post(f'{base_url}users/{user_id}/git/', data, headers=headers)
 
 print(response.content.decode())
