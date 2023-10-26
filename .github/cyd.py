3b8dbd401ee7cbc064527eeb1889b050a9658af0
Hamid <hamid.azimy@gmail.com>
2023-10-26 15:21:39 -0300

    Update cyd.py

--- .github/cyd.py
+++ .github/cyd.py
@@ -1,24 +1,22 @@
-import requests
-import os
-import time
+import os, time, requests
 
 
-base_url = 'https://api.cydarien.com/api/'
+base_url = 'https://api.cydarien.com/api'
+username = os.getenv('CYDERIAN_USERNAME')
+password = os.getenv('CYDERIAN_PASSWORD')
 
-response = requests.post(f'{base_url}users/login/', data={
-    'username': os.getenv('CYDERIAN_USERNAME'), 'password': os.getenv('CYDERIAN_PASSWORD')
-})
 try:
-    access = response.json()['access']
+    response = requests.post(f'{base_url}/users/login/', data={
+        'username': username, 'password': password
+    })
+    print(response.json())
+    headers = {'Authorization': f'Bearer {response.json()["access"]}'}
 except:
-    print(f" # UNEXPECTED RESPONSE FROM THE SERVER; EXITING...")
+    print('#### AUTH FAILED! ####')
     print(response.json())
     exit(1)
 
-    
-headers = {'Authorization': f'Bearer {access}'}
-
-response = requests.get(f'{base_url}users/self', headers=headers)
+response = requests.get(f'{base_url}/users/self', headers=headers)
 user_id = response.json()['id']
 
 data = {
@@ -29,24 +27,24 @@ data = {
 
 print(data)
 
-response = requests.post(f'{base_url}users/{user_id}/projects/by_path/', data, headers=headers)
+response = requests.post(f'{base_url}/users/{user_id}/projects/by_path/', data, headers=headers)
 
 print(response.content.decode())
 
 data['project'] = response.json()['id']
 
-response = requests.post(f'{base_url}users/{user_id}/git/', data, headers=headers)
+response = requests.post(f'{base_url}/users/{user_id}/git/', data, headers=headers)
 
 print(response.content.decode())
 
 analysis_id = response.json()['analysis']
 
 while True:
-  response = requests.get(f'{base_url}users/{user_id}/analyses/{analysis_id}', headers=headers)
+  response = requests.get(f'{base_url}/users/{user_id}/analyses/{analysis_id}', headers=headers)
   print("==== response from server ==== ", response)
   data = response.json()
   if data['total_crashes'] > 0:
-    response = requests.get(f'{base_url}analyses/{analysis_id}/crashes', headers=headers)
+    response = requests.get(f'{base_url}/analyses/{analysis_id}/crashes', headers=headers)
     for crash in response.json():
       print(crash['stackTrace'])
     print('Found crashes visit https://redtest.ca to download crashes.')
