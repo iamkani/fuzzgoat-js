1e6a45dfbe6155c5c502d14d6cf3976d3f2d8d74
Hamid <hamid.azimy@gmail.com>
2023-10-27 13:26:20 -0300

    Update cyd.py

--- .github/cyd.py
+++ .github/cyd.py
@@ -1,61 +1,44 @@
 import os, time, requests
 
 
-base_url = 'https://api.cydarien.com/api'
+api_url = 'https://api.cydarien.com/api'
 username = os.getenv('CYDERIAN_USERNAME')
 password = os.getenv('CYDERIAN_PASSWORD')
-
-
 host = os.getenv('REPOSITORY_HOST')
 repo = os.getenv('REPOSITORY_PATH')
+commit = os.getenv('COMMIT_SHA')
+headers = {}
+project_id = None
 
-print(host, repo)
-
-try:
-    response = requests.post(f'{base_url}/users/login/', data={
+def authenticate():
+    global headers
+    response = requests.post(f'{api_url}/users/login/', data={
         'username': username, 'password': password
     })
-    print(response.json())
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError:
+        print('############  AUTHENTICATION FAILED!  ############')
+        print(response.json()['detail'])
+        print('##################################################')
+        exit(1)
     headers = {'Authorization': f'Bearer {response.json()["access"]}'}
-except:
-    print('#### AUTH FAILED! ####')
-    print(response.json())
-    exit(1)
 
-response = requests.get(f'{base_url}/users/self', headers=headers)
-user_id = response.json()['id']
+def get_project_id():
+    global project_id
+    response = requests.post(f'{api_url}/users/self/projects/by_path/',
+        headers=headers,
+        data={
+            'host': host,
+            'repo': repo,
+        })
+    if response.status_code != 200:
+        print(response.json()['detail'])
+        exit(1)
+    project_id = response.json()['id']
 
-data = {
-    'repo': os.getenv('GITHUB_REPOSITORY'),
-    'commit': os.getenv('GITHUB_SHA'),
-    'host': 'GitHub',
-}
 
-print(data)
-
-response = requests.post(f'{base_url}/users/{user_id}/projects/by_path/', data, headers=headers)
-
-print(response.content.decode())
-
-data['project'] = response.json()['id']
-
-response = requests.post(f'{base_url}/users/{user_id}/git/', data, headers=headers)
-
-print(response.content.decode())
-
-analysis_id = response.json()['analysis']
-
-while True:
-  response = requests.get(f'{base_url}/users/{user_id}/analyses/{analysis_id}', headers=headers)
-  print("==== response from server ==== ", response)
-  data = response.json()
-  if data['total_crashes'] > 0:
-    response = requests.get(f'{base_url}/analyses/{analysis_id}/crashes', headers=headers)
-    for crash in response.json():
-      print(crash['stackTrace'])
-    print('Found crashes visit https://redtest.ca to download crashes.')
-    exit(1)
-  if data['status'] == 'T':
-    exit(0)
-  time.sleep(30)
-
+if __name__ == "__main__":
+    authenticate()
+    get_project_id()
+    print(project_id)
