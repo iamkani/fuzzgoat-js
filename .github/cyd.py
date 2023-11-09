a13b404a56e91ea8e73f382d9100d0bc0c03c38a
Hamid <hamid.azimy@gmail.com>
2023-11-09 09:35:14 -0400

    Update cyd.py

--- .github/cyd.py
+++ .github/cyd.py
@@ -2,13 +2,14 @@ import os, time, requests
 
 
 api_url = 'https://api.cydarien.com/api'
-username = os.getenv('CYDERIAN_USERNAME')
-password = os.getenv('CYDERIAN_PASSWORD')
+username = os.getenv('CYDARIEN_USERNAME')
+password = os.getenv('CYDARIEN_PASSWORD')
 host = os.getenv('REPOSITORY_HOST')
 repo = os.getenv('REPOSITORY_PATH')
 commit = os.getenv('COMMIT_SHA')
 headers = {}
 project_id = None
+analysis_id = None
 
 def authenticate():
     global headers
@@ -37,8 +38,32 @@ def get_project_id():
         exit(1)
     project_id = response.json()['id']
 
+def initiate_analysis():
+    global analysis_id
+    response = requests.post(f'{api_url}/users/self/git/',
+        headers=headers,
+        data={
+            'project': project_id,
+            'commit': commit
+        })
+    analysis_id = response.json()['analysis']
+
+def start_polling():
+    while True:
+        response = requests.get(f'{api_url}/users/self/analyses/{analysis_id}', headers=headers)
+        data = response.json()
+        if data['total_crashes'] > 0:
+            response = requests.get(f'{api_url}/analyses/{analysis_id}/crashes', headers=headers)
+            for crash in response.json():
+                print(crash['stackTrace'])
+                print('Found crashes visit https://fuzzer.cydarien.com to download crashes.')
+                exit(1)
+        if data['status'] == 'T':
+            exit(0)
+        time.sleep(30)
 
 if __name__ == "__main__":
     authenticate()
     get_project_id()
-    print(project_id)
+    initiate_analysis()
+    start_polling()
