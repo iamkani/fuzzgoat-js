d104bab19ab1b5e9d614cb8f5346fc61c6840c75
iamkani <arunkaniyamattam@gmail.com>
2024-03-07 16:20:54 -0400

    Update fuzz.js
    Test fuzz commit

--- fuzz.js
+++ fuzz.js
@@ -6,7 +6,7 @@ function fuzz(buf) {
     } catch (e) {
         // Those are "valid" exceptions. we can't catch them in one line as
         // jpeg-js doesn't export/inherit from one exception class/style.
-        //Test Comments
+        //Test Comment
         if (e.message.indexOf('JPEG') !== -1 ||
             e.message.indexOf('length octect') !== -1 ||
             e.message.indexOf('Failed to') !== -1 ||
